a907b268dd4019dbe90d4f06d36da1e949d2814a
Douglas Camargo <camargo.douglas@icloud.com>
2020-03-24 14:33:08 -0300

    setting routes and controllers

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,34 @@
 const express = require('express');
-
 const routes = express.Router();
+const OngController = require('./Controllers/OngController');
+const IncidentController = require('./Controllers/IncidentController');
+const ProfileController = require('./Controllers/ProfileController');
+const SessionController = require('./Controllers/SessionController');
+const connection = require('./database/connection');
 
-routes.post('/users', (req, res) => {
-  const body = req.body;
+routes.delete('/deleteAll', async (req, res) => {
 
-  console.log(body);
-  return res.json({
-    evento:  "Oministack 11",
-    aluno: "Diego Fernandes"
-  });
+    await connection('ongs').del();
+    await connection('incidents').del();
+    return res.status(201).json({
+      Deleted: "All"
+    })
 });
 
+routes.post('/sessions', SessionController.create );
+
+routes.get('/ongs', OngController.index ); 
+
+routes.post('/ongs', OngController.create );
+
+routes.delete('/ongs/:id', OngController.delete );
+
+routes.post('/incidents', IncidentController.create );
+
+routes.get('/incidents', IncidentController.index );
+
+routes.delete('/incidents/:id', IncidentController.delete );
+
+routes.get('/profile', ProfileController.index );
 
 module.exports = routes;
